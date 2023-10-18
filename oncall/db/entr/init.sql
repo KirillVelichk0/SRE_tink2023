0123fbc0dda4a0ae953e554145bdb7af743e61f1
Kirill <cirill@live.ru>
2023-10-18 17:38:22 +0300

    HW11 done

--- oncall/db/entr/init.sql
+++ oncall/db/entr/init.sql
@@ -1,3 +1,5 @@
 CREATE USER 'exporter'@'%' IDENTIFIED BY '1f1te123fq';
 GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'exporter'@'%';
 GRANT SELECT ON performance_schema.* TO 'exporter'@'%';
+SET GLOBAL general_log = 'ON';
+SET GLOBAL slow_query_log = 'ON';
